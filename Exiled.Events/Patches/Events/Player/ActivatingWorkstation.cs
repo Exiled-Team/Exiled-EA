c76b8db67c5fd307ceb6a0f611d09b8e71955bf1
Pietro <iopietro@hotmail.it>
2022-12-13 17:00:24 +0100

    Simplified ActivatingWorkstation patch, closed #77

--- Exiled.Events/Patches/Events/Player/ActivatingWorkstation.cs
+++ Exiled.Events/Patches/Events/Player/ActivatingWorkstation.cs
@@ -32,43 +32,52 @@ namespace Exiled.Events.Patches.Events.Player
         {
             List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
 
+            Label returnLabel = generator.DefineLabel();
+
+            LocalBuilder ev = generator.DeclareLocal(typeof(ActivatingWorkstationEventArgs));
+
             int offset = -1;
             int index = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Ldc_I4_1) + offset;
-            Label returnLabel = generator.DefineLabel();
-            LocalBuilder ev = generator.DeclareLocal(typeof(ActivatingWorkstationEventArgs));
+
+            // Remove "this.NetworkStatus = 1", to inject our custom logic
+            newInstructions.RemoveRange(index, 3);
 
             newInstructions.InsertRange(
                 index,
                 new CodeInstruction[]
                 {
+                    // Player.Get(ply)
                     new(OpCodes.Ldarg_1),
                     new(OpCodes.Call, Method(typeof(Player), nameof(Player.Get), new[] { typeof(ReferenceHub) })),
+
+                    // this
                     new(OpCodes.Ldarg_0),
+
+                    // true
                     new(OpCodes.Ldc_I4_1),
+
+                    // ActivatingWorkstationEventArgs ev = new(Player, WorkstationController, bool)
                     new(OpCodes.Newobj, GetDeclaredConstructors(typeof(ActivatingWorkstationEventArgs))[0]),
                     new(OpCodes.Dup),
                     new(OpCodes.Dup),
                     new(OpCodes.Stloc_S, ev.LocalIndex),
+
+                    // Handlers.Player.OnActivatingWorkstation(ev)
                     new(OpCodes.Call, Method(typeof(Handlers.Player), nameof(Handlers.Player.OnActivatingWorkstation))),
+
+                    // if (!ev.IsAllowed)
+                    //    return;
                     new(OpCodes.Callvirt, PropertyGetter(typeof(ActivatingWorkstationEventArgs), nameof(ActivatingWorkstationEventArgs.IsAllowed))),
                     new(OpCodes.Brfalse_S, returnLabel),
+
+                    // this.NetworkStatus = ev.NewStatus
                     new(OpCodes.Ldarg_0),
                     new(OpCodes.Ldloc_S, ev.LocalIndex),
                     new(OpCodes.Call, PropertyGetter(typeof(ActivatingWorkstationEventArgs), nameof(ActivatingWorkstationEventArgs.NewStatus))),
                     new(OpCodes.Call, PropertySetter(typeof(WorkstationController), nameof(WorkstationController.NetworkStatus))),
                 });
 
-            offset = -1;
-            index = newInstructions.FindLastIndex(instruction => instruction.opcode == OpCodes.Ldc_I4_1) + offset;
-
-            int moveOffset = -5;
-            int moveIndex = newInstructions.FindIndex(instruction => instruction.opcode == OpCodes.Newobj) + moveOffset;
-
-            newInstructions[index].MoveLabelsTo(newInstructions[moveIndex]);
-
-            newInstructions.RemoveRange(index, 3);
-
-            newInstructions[newInstructions.Count - 1].labels.Add(returnLabel);
+            newInstructions[newInstructions.Count - 1].WithLabels(returnLabel);
 
             for (int z = 0; z < newInstructions.Count; z++)
                 yield return newInstructions[z];
