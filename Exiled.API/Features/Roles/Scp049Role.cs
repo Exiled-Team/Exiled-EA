4633a95521f566ded88b621263e450b4ee7e6fd2
Thunder <thundermaker300@gmail.com>
2022-12-15 17:59:07 -0500

    Roles API (#123)
    * Add SCP role API
    
    * Fix certain broken statements
    
    * Update Scp939Role.cs
    
    * Update zombie to use API
    
    * API for Scp049 and Scp096
    
    * Rename ClawCooldown to AttackCooldown for consistency
    
    * Update Scp049Role.cs
    
    * Remove unused usings
    
    * Update Ragdoll.AllowRecall

--- Exiled.API/Features/Roles/Scp049Role.cs
+++ Exiled.API/Features/Roles/Scp049Role.cs
@@ -27,7 +27,7 @@ namespace Exiled.API.Features.Roles
         public Scp049Role(Player owner)
             : base(owner)
         {
-            SubroutineModule = (Owner.RoleManager.CurrentRole as Scp049GameRole).SubroutineModule;
+            SubroutineModule = (Base as Scp049GameRole).SubroutineModule;
         }
 
         /// <inheritdoc/>
@@ -42,7 +42,7 @@ namespace Exiled.API.Features.Roles
         public bool IsRecalling => SubroutineModule.TryGetSubroutine(out Scp049ResurrectAbility ability) && ability.IsInProgress;
 
         /// <summary>
-        /// Gets the player that is currently being revived by SCP-049. Will be <see langword="null"/> if <see cref="IsRecalling"/> is false.
+        /// Gets the player that is currently being revived by SCP-049. Will be <see langword="null"/> if <see cref="IsRecalling"/> is <see langword="false"/>.
         /// </summary>
         public Player RecallingPlayer
         {
@@ -56,13 +56,63 @@ namespace Exiled.API.Features.Roles
         }
 
         /// <summary>
-        /// Gets a boolean indicating whether or not SCP-049 is close enough to a ragdoll to revive it.
-        /// <para>
-        /// This method only returns whether or not SCP-049 is close enough to the body to revive it; the body may have expired. Make sure to check <see cref="Ragdoll.AllowRecall"/> to ensure the body can be revived.
-        /// </para>
+        /// Gets the ragdoll that is currently being revived by SCP-049. Will be <see langword="null"/> if <see cref="IsRecalling"/> is <see langword="false"/>.
         /// </summary>
+        public Ragdoll RecallingRagdoll
+        {
+            get
+            {
+                if (!IsRecalling || !SubroutineModule.TryGetSubroutine(out Scp049ResurrectAbility ability))
+                    return null;
+
+                return Ragdoll.Get(ability.CurRagdoll);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the amount of time before SCP-049 can use its Doctor's Call ability again.
+        /// </summary>
+        public float CallCooldown
+        {
+            get => SubroutineModule.TryGetSubroutine(out Scp049CallAbility ability) ? ability.Cooldown.Remaining : 0f;
+            set
+            {
+                if (SubroutineModule.TryGetSubroutine(out Scp049CallAbility ability))
+                {
+                    ability.Cooldown.Remaining = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the amount of time before SCP-049 can use its Good Sense ability again.
+        /// </summary>
+        public float AmnesticCloudCooldown
+        {
+            get => SubroutineModule.TryGetSubroutine(out Scp049SenseAbility ability) ? ability.Cooldown.Remaining : 0f;
+            set
+            {
+                if (SubroutineModule.TryGetSubroutine(out Scp049SenseAbility ability))
+                {
+                    ability.Cooldown.Remaining = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns a <see langword="bool"/> indicating whether or not SCP-049 is close enough to a ragdoll to revive it.
+        /// </summary>
+        /// <remarks>This method only returns whether or not SCP-049 is close enough to the body to revive it; the body may have expired. Make sure to check <see cref="Ragdoll.AllowRecall"/> to ensure the body can be revived.</remarks>
         /// <param name="ragdoll">The ragdoll to check.</param>
         /// <returns><see langword="true"/> if close enough to revive the body; otherwise, <see langword="false"/>.</returns>
-        public bool IsInRecallRange(Ragdoll ragdoll) => Vector3.Distance(Owner.ReferenceHub.transform.position, ragdoll.Position) <= Scp049.ReviveDistance * 1.3f;
+        public bool IsInRecallRange(BasicRagdoll ragdoll) => SubroutineModule.TryGetSubroutine(out Scp049ResurrectAbility ability) ? ability.IsCloseEnough(Owner.Position, ragdoll.transform.position) : false;
+
+        /// <summary>
+        /// Returns a <see langword="bool"/> indicating whether or not SCP-049 is close enough to a ragdoll to revive it.
+        /// </summary>
+        /// <remarks>This method only returns whether or not SCP-049 is close enough to the body to revive it; the body may have expired. Make sure to check <see cref="Ragdoll.AllowRecall"/> to ensure the body can be revived.</remarks>
+        /// <param name="ragdoll">The ragdoll to check.</param>
+        /// <returns><see langword="true"/> if close enough to revive the body; otherwise, <see langword="false"/>.</returns>
+        public bool IsInRecallRange(Ragdoll ragdoll) => IsInRecallRange(ragdoll.Base);
     }
 }
--- Exiled.API/Features/Roles/Scp939Role.cs
+++ Exiled.API/Features/Roles/Scp939Role.cs
@@ -10,6 +10,7 @@ namespace Exiled.API.Features.Roles
     using System.Collections.Generic;
 
     using PlayerRoles;
+    using PlayerRoles.PlayableScps.Scp939;
     using PlayerRoles.PlayableScps.Subroutines;
 
     using Scp939GameRole = PlayerRoles.PlayableScps.Scp939.Scp939Role;
@@ -26,7 +27,7 @@ namespace Exiled.API.Features.Roles
         public Scp939Role(Player owner)
             : base(owner)
         {
-            SubroutineModule = (Owner.RoleManager.CurrentRole as Scp939GameRole).SubroutineModule;
+            SubroutineModule = (Base as Scp939GameRole).SubroutineModule;
         }
 
         /// <inheritdoc/>
@@ -40,8 +41,53 @@ namespace Exiled.API.Features.Roles
         /// </summary>
         public float AttackCooldown
         {
-            get => 0.6f; // It's hardcoded
-            set { }
+            get => SubroutineModule.TryGetSubroutine(out Scp939ClawAbility ability) ? ability.Cooldown.Remaining : 0f;
+            set
+            {
+                if (SubroutineModule.TryGetSubroutine(out Scp939ClawAbility ability))
+                {
+                    ability.Cooldown.Remaining = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether or not SCP-939 is currently using its focus ability.
+        /// </summary>
+        public bool IsFocused => SubroutineModule.TryGetSubroutine(out Scp939FocusAbility focus) ? focus.TargetState : false;
+
+        /// <summary>
+        /// Gets a value indicating whether or not SCP-939 is currently lunging.
+        /// </summary>
+        public bool IsLunging => SubroutineModule.TryGetSubroutine(out Scp939LungeAbility ability) ? ability.State != Scp939LungeState.None : false;
+
+        /// <summary>
+        /// Gets or sets SCP-939's <see cref="Scp939LungeState"/>.
+        /// </summary>
+        public Scp939LungeState State
+        {
+            get => SubroutineModule.TryGetSubroutine(out Scp939LungeAbility ability) ? ability.State : Scp939LungeState.None;
+            set
+            {
+                if (!SubroutineModule.TryGetSubroutine(out Scp939LungeAbility ability))
+                    return;
+                ability.State = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the amount of time before SCP-939 can use its amnestic cloud ability again.
+        /// </summary>
+        public float AmnesticCloudCooldown
+        {
+            get => SubroutineModule.TryGetSubroutine(out Scp939AmnesticCloudAbility ability) ? ability.Cooldown.Remaining : 0f;
+            set
+            {
+                if (SubroutineModule.TryGetSubroutine(out Scp939AmnesticCloudAbility ability))
+                {
+                    ability.Cooldown.Remaining = value;
+                }
+            }
         }
 
         /// <summary>
