7dcc89f8af5ea9225c6576fdd5d6db3ac3c7ce9e
Thunder <thundermaker300@gmail.com>
2022-12-19 11:40:00 -0500

    Add SCP-049-2 API

--- Exiled.API/Features/Roles/Scp0492Role.cs
+++ Exiled.API/Features/Roles/Scp0492Role.cs
@@ -41,6 +41,24 @@ namespace Exiled.API.Features.Roles
         }
 
         /// <summary>
+        /// Gets a value indicating whether or not SCP-049-2 is currently pursuing a target (Bloodlust ability).
+        /// </summary>
+        public bool BloodlustActive
+        {
+            get => SubroutineModule.TryGetSubroutine(out ZombieBloodlustAbility ability) ? ability.LookingAtTarget : false;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether or not SCP-049-2 is consuming a ragdoll.
+        /// </summary>
+        public bool IsConsuming => SubroutineModule.TryGetSubroutine(out ZombieConsumeAbility ability) ? ability.IsInProgress : false;
+
+        /// <summary>
+        /// Gets the <see cref="Ragdoll"/> that SCP-049-2 is currently consuming. Will be <see langword="null"/> if <see cref="IsConsuming"/> is <see langword="false"/>.
+        /// </summary>
+        public Ragdoll RagdollConsuming => SubroutineModule.TryGetSubroutine(out ZombieConsumeAbility ability) ? Ragdoll.Get(ability.CurRagdoll) : null;
+
+        /// <summary>
         /// Gets the amount of time in between SCP-049-2 attacks.
         /// </summary>
         public float AttackCooldown
