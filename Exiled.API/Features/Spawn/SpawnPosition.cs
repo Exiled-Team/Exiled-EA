4e54587c978c3da0afe767cce83728504c328003
Thunder <thundermaker300@gmail.com>
2022-12-22 22:06:45 -0500

    Update Exiled.API/Features/Spawn/SpawnPosition.cs
    Make SpawnPosition constructor public
    
    Co-authored-by: Pietro <iopietro@hotmail.it>

--- Exiled.API/Features/Spawn/SpawnPosition.cs
+++ Exiled.API/Features/Spawn/SpawnPosition.cs
@@ -21,7 +21,7 @@ namespace Exiled.API.Features.Spawn
         /// <param name="roleType">The <see cref="RoleTypeId"/> this spawn is for.</param>
         /// <param name="position">The <see cref="Vector3"/> position of the spawn.</param>
         /// <param name="horizontalRotation">The horizontal rotation of the spawn.</param>
-        internal SpawnPosition(RoleTypeId roleType, Vector3 position, float horizontalRotation)
+        public SpawnPosition(RoleTypeId roleType, Vector3 position, float horizontalRotation)
         {
             RoleType = roleType;
             Position = position;
